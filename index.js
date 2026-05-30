bbeb09a98ec90d685362af5499c8c4aae733e4b9
Anish Kuamr <kumaranishabd07@gmail.com>
2026-05-30 14:19:53 +0530

    Feature:Basic app setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+const env = require("dotenv")
+env.config()
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+
+app.get("/home" , (req,res) => {
+    console.log("Hitted Homes")
+    return res.json({
+        success:true,
+        message:"Fetched Home"
+    })
+})
+
+app.listen(process.env.PORT , async() => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+
+    try{
+        await mongoose.connect(process.env.DB_URL)
+        console.log("Succesfully connected to mongodb server")
+
+    }
+    catch(err){
+        console.log(`error while connecting to db ${err}`)
+    }
+})
