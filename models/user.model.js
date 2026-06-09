7d4eb85fd1d0b9ec1988872eb430c5809e8306d1
Anish Kuamr <kumaranishabd07@gmail.com>
2026-06-09 16:54:40 +0530

    Feat:Convert plain password to encrypted password

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -30,6 +31,12 @@ const userSchema = new mongoose.Schema({
         default:"APPROVED"
     }
 },{timestamps:true});
+
+userSchema.pre("save",async function(){
+    const hash = await bcrypt.hash(this.password,10);
+    this.password = hash;
+    
+})
 
 const User = mongoose.model("User",userSchema);
 
