bc3f1e2a960489cc0a19cee43b12675f84cd4915
Anish Kuamr <kumaranishabd07@gmail.com>
2026-06-15 17:12:09 +0530

    Feat:Create Booking model

---
+++ models/booking.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const {BOOKING_STATUS} = require("../utils/constants")
+
+const bookingSchema = new mongoose.Schema({
+    theatreId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Theatre",
+        required:true
+    },
+    movieId:{
+        type:mongoose.Schema.types.ObjectId,
+        ref:"Movie",
+        required:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    timings:{
+        type:String,
+        required:true
+    },
+    noOfSeats:{
+        type:Number
+    },
+    status:{
+        type:String,
+        required:true,
+        enum:{
+            values:[BOOKING_STATUS.processing,BOOKING_STATUS.cancelled,BOOKING_STATUS.successfull],
+            message:"Invalid Status"
+        },
+        default:BOOKING_STATUS.processing
+    }
+
+},{timestamps:true});
+
+const booking = mongoose.model("booking",bookingSchema);
+
+module.exports = booking;
